e00be8ce0fcd2f070228edf2122c17930a32a2fd
Dmitriy <dimedrol913@gmail.com>
2022-06-20 15:38:20 +0300

    Belokur find file on localhost recursively

--- main.cpp
+++ main.cpp
@@ -4,20 +4,27 @@
 #include <dirent.h>
 #include <cstring>
 #include <regex>
-// constexpr глобал количество сред 
+ 
 ////////////////////////////////////////////////////////////////
-void ListDirectory(const char* name_directory);
+void ListDirectory(const char* name_directory, const char* file_name);
 
 ////////////////////////////////////////////////////////////////
-int main() {
-    const char* CONST_main_dir = "/home/dmiriy/";
-    ListDirectory(CONST_main_dir);
+int main(int argc, char *argv[]) {
+    const char* CONST_main_dir = "/";
+    char file_name[256];
+
+    std::cout << "Please enter enter the name of the file you are looking for.\n";
+    std::cin >> file_name;
+    std::cout << "\n";
+
+    const char* f_name = file_name; 
+    ListDirectory(CONST_main_dir,f_name);
     
     return 0;
 }
 
 ////////////////////////////////////////////////////////////////
-void ListDirectory(const char* name_directory){
+void ListDirectory(const char* name_directory, const char* file_name){
     DIR *dir;
     struct dirent *entry;  
     dir = opendir(name_directory);
@@ -38,10 +45,12 @@ void ListDirectory(const char* name_directory){
         strcat(full_d_name, entry->d_name);         
         
         if (std::regex_search(CONST_d_name, result, reqular)){
-            std::cout << CONST_d_name << " " << full_d_name << "\n";
+            if (strcmp(CONST_d_name, file_name) == 0){
+                std::cout << full_d_name << "\n";
+            } 
             continue;
         } 
-            
+      
         strcat(full_d_name, CONST_split_dir);
         const char* CONST_sub_dir = full_d_name;
         
@@ -51,7 +60,7 @@ void ListDirectory(const char* name_directory){
         //std::thread th(ListDirectory,CONST_sub_dir);
         //th.join();
 
-        ListDirectory(CONST_sub_dir);
+        ListDirectory(CONST_sub_dir, file_name);
     }
     return;
 }
